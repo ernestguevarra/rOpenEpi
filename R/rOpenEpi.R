4734090e2964753eb2cfe65d78c76e4d9d4ce49c
ernestguevarra <ernest@guevarra.io>
2020-02-08 06:41:45 +0000

    create package documentation

---
+++ R/rOpenEpi.R
@@ -0,0 +1,25 @@
+################################################################################
+#
+#' R Port of Open Source Epidemiologic Statistics for Public Health
+#'
+#' This package is the R port for the free and open source software OpenEpi
+#' <https://www.openepi.com>. OpenEpi provides statistics for counts and
+#' measurements in descriptive and analytic studies, stratified analysis with
+#' exact confidence limits, matched pair and person-time analysis, sample size
+#' and power calculations, random numbers, sensitivity, specificity and other
+#' evaluation statistics, R x C tables and chi-square for dose-response. The
+#' original software was written in JavaScript and HTML and was designed to be
+#' a serverless, browser-based application that can be used on any computer
+#' platform including smartphones. With the advent of Shiny in R, re-creating
+#' the OpenEpi browser-based application has become possible. This package
+#' translates the JavaScript calculator functions used in the original
+#' application into R and includes a Shiny application that uses these functions
+#' to replicate the original OpenEpi browser application.
+#'
+#' @docType package
+#' @name rOpenEpi
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
